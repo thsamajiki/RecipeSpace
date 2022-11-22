5a1e90f6f7b6bc7c4ca2a475124e762f04116c2c
thsamajiki <ant1102@naver.com>
2022-11-22 22:13:38 +0900

    Dao, AppDatabase 추가

--- app/src/main/java/com/hero/recipespace/database/recipe/dao/RecipeDao.kt
+++ app/src/main/java/com/hero/recipespace/database/recipe/dao/RecipeDao.kt
@@ -8,7 +8,7 @@ import com.hero.recipespace.data.recipe.RecipeData
 @Dao
 interface RecipeDao {
     @Query("SELECT * FROM recipe_db ORDER BY key ASC")
-    fun getAllRecipes() : LiveData<List<RecipeData>>
+    fun getAllRecipes() : LiveData<List<RecipeData>> //
 
     @Query("SELECT * FROM recipe_db WHERE key = :key limit 1")
     suspend fun getRecipeFromKey(key: String?): RecipeData?
