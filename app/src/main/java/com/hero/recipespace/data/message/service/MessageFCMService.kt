34537adf9b4ebaa0874e8181ebfdfe6e3b4a9ea8
thsamajiki <ant1102@naver.com>
2023-01-05 12:39:08 +0900

    minSdk 를 27 -> 23으로 변경

--- app/src/main/java/com/hero/recipespace/data/message/service/MessageFCMService.kt
+++ app/src/main/java/com/hero/recipespace/data/message/service/MessageFCMService.kt
@@ -1,19 +1,8 @@
 package com.hero.recipespace.data.message.service
 
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
-import android.net.Uri
-import android.os.Build
-import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.hero.recipespace.R
-import com.hero.recipespace.view.main.chat.ChatActivity
 
 
 class MessageFCMService: FirebaseMessagingService() {
@@ -33,33 +22,33 @@ class MessageFCMService: FirebaseMessagingService() {
     }
 
     private fun sendNotification(title: String, messageBody: String) {
-        val intent = ChatActivity.getIntent(this)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-
-        val channelId = getString(R.string.app_name) // R.string.default_notification_channel_id
-        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder: NotificationCompat.Builder =
-            NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_camera)
-                .setContentTitle(title)
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // Since android Oreo notification channel is needed.
-
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("fcm_default_channel",
-                "fcm_default_channel",
-                NotificationManager.IMPORTANCE_DEFAULT)
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+//        val intent = ChatActivity.getIntent(this)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+//
+//        val channelId = getString(R.string.app_name) // R.string.default_notification_channel_id
+//        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val notificationBuilder: NotificationCompat.Builder =
+//            NotificationCompat.Builder(this, channelId)
+//                .setSmallIcon(R.drawable.ic_camera)
+//                .setContentTitle(title)
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent)
+//
+//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//
+//        // Since android Oreo notification channel is needed.
+//
+//        // Since android Oreo notification channel is needed.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel("fcm_default_channel",
+//                "fcm_default_channel",
+//                NotificationManager.IMPORTANCE_DEFAULT)
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//
+//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 }
