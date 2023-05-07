c3cac88c422cdcdc129e8ce371d3d8fcb68a6a87
thsamajiki <ant1102@naver.com>
2023-05-08 02:16:03 +0900

    공지사항 조회 오류 수정

--- app/src/main/java/com/hero/recipespace/domain/notice/usecase/ObserveNoticeListUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/notice/usecase/ObserveNoticeListUseCase.kt
@@ -2,11 +2,12 @@ package com.hero.recipespace.domain.notice.usecase
 
 import com.hero.recipespace.domain.notice.entity.NoticeEntity
 import com.hero.recipespace.domain.notice.repository.NoticeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ObserveNoticeListUseCase @Inject constructor(
     private val noticeRepository: NoticeRepository
 ) {
-    suspend operator fun invoke() : List<NoticeEntity> =
+    operator fun invoke() : Flow<List<NoticeEntity>> =
         noticeRepository.getNoticeList()
 }
