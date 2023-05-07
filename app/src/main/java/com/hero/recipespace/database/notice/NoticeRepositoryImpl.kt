37a76b1939f68eaf8c6c530de06db7d444fdbfe3
thsamajiki <ant1102@naver.com>
2023-05-08 02:16:25 +0900

    공지사항 조회 오류 수정

--- app/src/main/java/com/hero/recipespace/database/notice/NoticeRepositoryImpl.kt
+++ app/src/main/java/com/hero/recipespace/database/notice/NoticeRepositoryImpl.kt
@@ -5,6 +5,8 @@ import com.hero.recipespace.data.notice.remote.NoticeRemoteDataSource
 import com.hero.recipespace.domain.notice.entity.NoticeEntity
 import com.hero.recipespace.domain.notice.mapper.toEntity
 import com.hero.recipespace.domain.notice.repository.NoticeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class NoticeRepositoryImpl @Inject constructor(
@@ -14,10 +16,12 @@ class NoticeRepositoryImpl @Inject constructor(
         return noticeRemoteDataSource.getData(noticeKey).toEntity()
     }
 
-    override suspend fun getNoticeList(): List<NoticeEntity> {
+    override fun getNoticeList(): Flow<List<NoticeEntity>> {
         return noticeRemoteDataSource.getDataList()
-            .map {
-                it.toEntity()
+            .map { it ->
+                it.map {
+                    it.toEntity()
+                }
             }
     }
 
