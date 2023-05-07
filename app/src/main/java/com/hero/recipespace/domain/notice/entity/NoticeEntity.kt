e1344469079dd3b0c959aee39eeee57e5b818609
thsamajiki <ant1102@naver.com>
2023-05-08 02:17:03 +0900

    공지사항 조회 오류 수정

--- app/src/main/java/com/hero/recipespace/domain/notice/entity/NoticeEntity.kt
+++ app/src/main/java/com/hero/recipespace/domain/notice/entity/NoticeEntity.kt
@@ -1,6 +1,7 @@
 package com.hero.recipespace.domain.notice.entity
 
 import android.os.Parcelable
+import com.google.firebase.Timestamp
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -8,7 +9,7 @@ data class NoticeEntity(
     var key: String = "",
     var title: String? = null,
     var desc: String? = null,
-    var postDate: String? = null
+    var postDate: Timestamp? = null
 ) : Parcelable {
 
 }
