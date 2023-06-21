46398e8588c439a0cd522e8f0ef8217ead65b0aa
thsamajiki <ant1102@naver.com>
2023-06-22 00:23:00 +0900

    불필요한 메소드 제거

--- app/src/main/java/com/hero/recipespace/storage/FirebaseStorageApi.kt
+++ app/src/main/java/com/hero/recipespace/storage/FirebaseStorageApi.kt
@@ -1,19 +1,12 @@
 package com.hero.recipespace.storage
 
-import android.net.Uri
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.hero.recipespace.listener.OnFileUploadListener
-import java.io.File
 
 class FirebaseStorageApi {
-    private var onFileUploadListener: OnFileUploadListener? = null
     private val storageRef: StorageReference = FirebaseStorage.getInstance().reference
 
     companion object {
-        const val DEFAULT_IMAGE_PATH = "images/"
-        const val PROFILE_IMAGE_PATH = "profile/"
-
         private var instance: FirebaseStorageApi ?= null
 
         fun getInstance(): FirebaseStorageApi {
@@ -23,32 +16,5 @@ class FirebaseStorageApi {
                 }
             }
         }
-    }
-
-    fun setOnFileUploadListener(onFileUploadListener: OnFileUploadListener?) {
-        this.onFileUploadListener = onFileUploadListener
-    }
-
-    fun uploadImages(folderPath: String, filePath: String) {
-        val file = Uri.fromFile(File(filePath))
-        val storageRef =
-            FirebaseStorage.getInstance().reference.child(folderPath + file.lastPathSegment)
-        val uploadTask = storageRef.putFile(file)
-        uploadTask
-            .addOnProgressListener { taskSnapshot ->
-                val percent =
-                    taskSnapshot.bytesTransferred.toFloat() / taskSnapshot.totalByteCount.toFloat() * 100
-//                onFileUploadListener?.onFileUploadProgress(percent)
-            }.continueWithTask { task ->
-                if (!task.isSuccessful) {
-                    throw task.exception!!
-                }
-                storageRef.downloadUrl
-            }
-            .addOnSuccessListener { uri ->
-//                onFileUploadListener?.onFileUploadComplete(true, uri.toString())
-            }.addOnFailureListener {
-//                onFileUploadListener?.onFileUploadComplete(false, null)
-            }
     }
 }
