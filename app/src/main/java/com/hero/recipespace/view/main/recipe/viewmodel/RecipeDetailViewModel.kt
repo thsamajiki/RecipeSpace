d2818e6d7bfad16e8b60d8944d05258e331da4d9
thsamajiki <ant1102@naver.com>
2023-06-22 02:28:59 +0900

    레시피 삭제 기능 구현

--- app/src/main/java/com/hero/recipespace/view/main/recipe/viewmodel/RecipeDetailViewModel.kt
+++ app/src/main/java/com/hero/recipespace/view/main/recipe/viewmodel/RecipeDetailViewModel.kt
@@ -8,8 +8,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.Timestamp
 import com.hero.recipespace.domain.recipe.entity.RecipeEntity
+import com.hero.recipespace.domain.recipe.usecase.DeleteRecipeUseCase
 import com.hero.recipespace.domain.recipe.usecase.GetRecipeUseCase
+import com.hero.recipespace.view.LoadingState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,6 +22,14 @@ sealed class RecipeDetailUIState {
     data class Success(val recipeEntity: RecipeEntity) : RecipeDetailUIState()
 
     data class Failed(val message: String) : RecipeDetailUIState()
+}
+
+sealed class DeleteRecipeUiState {
+    object Success : DeleteRecipeUiState()
+
+    data class Failed(val message: String) : DeleteRecipeUiState()
+
+    object Idle : DeleteRecipeUiState()
 }
 
 sealed class IntentChatUIState {
@@ -34,7 +47,8 @@ sealed class OpenRateUIState {
 @HiltViewModel
 class RecipeDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getRecipeUseCase: GetRecipeUseCase
+    private val getRecipeUseCase: GetRecipeUseCase,
+    private val deleteRecipeUseCase: DeleteRecipeUseCase
 ) : ViewModel() {
 
     companion object {
@@ -44,6 +58,12 @@ class RecipeDetailViewModel @Inject constructor(
     private val _recipeDetailUiState = MutableLiveData<RecipeDetailUIState>()
     val recipeDetailUiState: LiveData<RecipeDetailUIState>
         get() = _recipeDetailUiState
+
+    private val _deleteRecipeUiState = MutableStateFlow<DeleteRecipeUiState>(DeleteRecipeUiState.Idle)
+    val deleteRecipeUiState: StateFlow<DeleteRecipeUiState> = _deleteRecipeUiState.asStateFlow()
+
+    private val _loadingState = MutableStateFlow<LoadingState>(LoadingState.Idle)
+    val loadingState: StateFlow<LoadingState> = _loadingState.asStateFlow()
 
     private val _recipe = MutableLiveData<RecipeEntity>()
     val recipe: LiveData<RecipeEntity>
@@ -71,12 +91,27 @@ class RecipeDetailViewModel @Inject constructor(
                     desc.value = it.desc.orEmpty()
                     rate.value = it.rate
                     photoUrlList.value = it.photoUrlList.orEmpty()
-                    Log.d("zxc", "RecipeDetailViewModel {recipe.value!!.userName}: ${recipe.value!!.userName}")
-                    Log.d("zxc", "RecipeDetailViewModel {recipe.value!!.profileImageUrl}: ${recipe.value!!.profileImageUrl}")
                 }
                 .onFailure {
                     it.printStackTrace()
                     Log.e("RecipeDetailViewModel", "$it ")
+                }
+        }
+    }
+
+    fun deleteRecipe(recipe: RecipeEntity) {
+        _loadingState.value = LoadingState.Loading
+
+        viewModelScope.launch {
+            deleteRecipeUseCase.invoke(recipe)
+                .onSuccess {
+                    _loadingState.value = LoadingState.Hidden
+                    _deleteRecipeUiState.value = DeleteRecipeUiState.Success
+                }
+                .onFailure {
+                    _loadingState.value = LoadingState.Hidden
+                    _deleteRecipeUiState.value = DeleteRecipeUiState.Failed(it.message.orEmpty())
+                    it.printStackTrace()
                 }
         }
     }
