821bb61f7094e5d65e34101401066c17154ed697
thsamajiki <ant1102@naver.com>
2023-06-22 00:21:55 +0900

    TimeUtils 에 시간 형식을 보완

--- app/src/main/java/com/hero/recipespace/ext/TextBindingAdapter.kt
+++ app/src/main/java/com/hero/recipespace/ext/TextBindingAdapter.kt
@@ -4,12 +4,27 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @BindingAdapter("timestamp")
 fun TextView.setTimestamp(timestamp: Timestamp?) {
     timestamp ?: return
 
-    val simpleDateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
-//    val simpleDateFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분")
-    text = simpleDateFormat.format(timestamp.toDate())
+    val calendar = Calendar.getInstance()
+
+    val currentDate = calendar.time.time
+    val diff = (currentDate - timestamp.toDate().time) / 1000
+    val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(timestamp.toDate())
+
+    text = when (diff) {
+        in 0 until 10 -> "지금 막"
+        in 10 until 60 -> "${diff}초 전"
+        in 60 until 60 * 60 -> "${diff / 60}분 전"
+        in 60 * 60 until 60 * 60 * 24 -> "${diff / (60 * 60)}시간 전"
+        in 60 * 60 * 24 until 60 * 60 * 48 -> "어제"
+        in 60 * 60 * 48 until 60 * 60 * 72 -> "그저께"
+        in 60 * 60 * 72 until 60 * 60 * 24 * 7 -> "${diff / (60 * 60 * 24)}일 전"
+        else -> dateFormat
+    }
 }
