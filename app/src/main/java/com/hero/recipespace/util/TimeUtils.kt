7c7f1e319dfe1c2f6ac6b67a5e2741ac0c95ab25
thsamajiki <ant1102@naver.com>
2023-07-11 00:55:04 +0900

    라인 정렬

--- app/src/main/java/com/hero/recipespace/util/TimeUtils.kt
+++ app/src/main/java/com/hero/recipespace/util/TimeUtils.kt
@@ -7,7 +7,6 @@ import java.util.Locale
 object TimeUtils {
     // 시간 관련 함수들을 클래스로 모아둠
 
-
     fun convertTimeFormat(date: Date?, format: String?): String? {
         date ?: return null
         val currentDate = Date(System.currentTimeMillis()).time
