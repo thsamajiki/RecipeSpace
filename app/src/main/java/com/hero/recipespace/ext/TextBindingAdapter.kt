a12912194f4104241700acf8d2b5797b75c4f90d
thsamajiki <ant1102@naver.com>
2023-12-25 21:49:57 +0900

    TextBindingAdapter에 메시지 날짜 및 시간 데이터 바인딩 추가

--- app/src/main/java/com/hero/recipespace/ext/TextBindingAdapter.kt
+++ app/src/main/java/com/hero/recipespace/ext/TextBindingAdapter.kt
@@ -5,6 +5,7 @@ import androidx.databinding.BindingAdapter
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 @BindingAdapter("timestamp")
@@ -27,4 +28,35 @@ fun TextView.setTimestamp(timestamp: Timestamp?) {
         in 60 * 60 * 72 until 60 * 60 * 24 * 7 -> "${diff / (60 * 60 * 24)}일 전"
         else -> dateFormat
     }
+}
+
+@BindingAdapter("messageTimestamp")
+fun TextView.setMessageTimestamp(timestamp: Timestamp?) {
+    timestamp ?: return
+
+    val milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+    val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault()).format(timestamp.toDate())
+    val netDate = Date(milliseconds)
+    val date = timeFormat.format(netDate)
+    val hour = date.split(":")[0]
+    val minute = date.split(":")[1]
+
+
+    text = when(hour.toInt()) {
+        in 0 until 12 -> "오전 ${hour}:${minute}"
+        12 -> "오후 ${hour}:${minute}"
+        else -> "오후 ${hour.toInt() - 12}:${minute}"
+    }
+}
+
+@BindingAdapter("messageDatestamp")
+fun TextView.setMessageDatestamp(timestamp: Timestamp?) {
+    timestamp ?: return
+
+    val calendar = Calendar.getInstance()
+
+    val currentDate = calendar.time.time
+    val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(timestamp.toDate())
+
+    text = dateFormat
 }
