04929b89a8874477cf2a4b54183cf4c668db97ac
thsamajiki <ant1102@naver.com>
2023-12-27 17:42:21 +0900

    SplashActivity의 각 View에 대한 애니메이션 구현

--- app/src/main/java/com/hero/recipespace/SplashActivity.kt
+++ app/src/main/java/com/hero/recipespace/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.hero.recipespace
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.hero.recipespace.databinding.ActivitySplashBinding
 import com.hero.recipespace.view.login.LoginActivity
 import com.hero.recipespace.view.viewmodel.SplashUIState
 import com.hero.recipespace.view.viewmodel.SplashViewModel
@@ -10,12 +14,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivitySplashBinding
     private val viewModel by viewModels<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
 
         viewModel.splashUiState.observe(this) { state ->
             when (state) {
@@ -32,5 +39,16 @@ class SplashActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun setupView() {
+        startAnimationWithShow(this, binding.ivCookingPan, R.anim.show01)
+        startAnimationWithShow(this, binding.ivBalloons, R.anim.show02)
+        startAnimationWithShow(this, binding.ivBottomCookLogo, R.anim.show03)
+    }
+
+    private fun startAnimationWithShow(context: Context, view: View, id: Int) {
+        view.visibility = View.VISIBLE  // 애니메이션 전에 뷰를 보이게 한다
+        view.startAnimation(AnimationUtils.loadAnimation(context, id)) // 애니메이션 설정 & 시작
     }
 }
