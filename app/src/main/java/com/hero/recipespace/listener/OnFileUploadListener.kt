a2178a6aac17b2628f2cfe1abe05cb28f3f932f0
thsamajiki <ant1102@naver.com>
2024-03-01 23:04:12 +0900

    파일 업로드 리스너 제거

--- app/src/main/java/com/hero/recipespace/listener/OnFileUploadListener.kt
+++
@@ -1,6 +0,0 @@
-package com.hero.recipespace.listener
-
-interface OnFileUploadListener {
-    suspend fun onFileUploadComplete(isSuccess: Boolean, downloadUrl: String?)
-    suspend fun onFileUploadProgress(percent: Float)
-}
