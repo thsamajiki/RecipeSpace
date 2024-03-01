aa6c220bce66534910b0549703fb5c0c19653f5c
thsamajiki <ant1102@naver.com>
2024-03-01 23:04:22 +0900

    메시지 전송 리스너 제거

--- app/src/main/java/com/hero/recipespace/listener/OnMessageListener.kt
+++
@@ -1,7 +0,0 @@
-package com.hero.recipespace.listener
-
-import com.hero.recipespace.data.message.MessageData
-
-interface OnMessageListener {
-    fun onMessage(isSuccess: Boolean, messageData: MessageData?)
-}
