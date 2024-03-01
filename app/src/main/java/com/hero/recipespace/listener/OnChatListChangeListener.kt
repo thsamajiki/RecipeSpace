5c4fd2d6213448c543f6ca2c5dd358edf162a528
thsamajiki <ant1102@naver.com>
2024-03-01 23:03:39 +0900

    채팅 목록 변화 리스너 제거

--- app/src/main/java/com/hero/recipespace/listener/OnChatListChangeListener.kt
+++
@@ -1,8 +0,0 @@
-package com.hero.recipespace.listener
-
-import com.google.firebase.firestore.DocumentChange
-import com.hero.recipespace.data.chat.ChatData
-
-interface OnChatListChangeListener {
-    fun onChatListChange(changeType: DocumentChange.Type?, chatData: ChatData)
-}
