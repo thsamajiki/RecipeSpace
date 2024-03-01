569616f75b8f7342737966637c2c3eca9284c7fa
thsamajiki <ant1102@naver.com>
2024-03-01 23:04:35 +0900

    공지사항 업데이트 리스너 제거

--- app/src/main/java/com/hero/recipespace/listener/OnNoticeListChangeListener.kt
+++
@@ -1,8 +0,0 @@
-package com.hero.recipespace.listener
-
-import com.google.firebase.firestore.DocumentChange
-import com.hero.recipespace.data.notice.NoticeData
-
-interface OnNoticeListChangeListener {
-    fun onNoticeListChange(changeType: DocumentChange.Type?, noticeData: NoticeData?)
-}
