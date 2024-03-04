ad3cc60230ac80eaf18213d2be66913973163fc0
thsamajiki <ant1102@naver.com>
2024-03-04 23:27:35 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/chat/usecase/CountUnreadMessageUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/chat/usecase/CountUnreadMessageUseCase.kt
@@ -3,8 +3,10 @@ package com.hero.recipespace.domain.chat.usecase
 import com.hero.recipespace.domain.chat.repository.ChatRepository
 import javax.inject.Inject
 
-class CountUnreadMessageUseCase @Inject constructor(
-    private val chatRepository: ChatRepository
+class CountUnreadMessageUseCase
+@Inject
+constructor(
+    private val chatRepository: ChatRepository,
 ) {
 //    suspend operator fun invoke(chatKey: String) =
 //        chatRepository.readMessage(chatKey)
