75092bb09778297443e784bcb3c3e7e4468df57a
thsamajiki <ant1102@naver.com>
2024-03-04 23:27:42 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/chat/usecase/CreateNewChatRoomUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/chat/usecase/CreateNewChatRoomUseCase.kt
@@ -4,8 +4,10 @@ import com.hero.recipespace.domain.chat.repository.ChatRepository
 import com.hero.recipespace.domain.chat.request.AddChatRequest
 import javax.inject.Inject
 
-class CreateNewChatRoomUseCase @Inject constructor(
-    private val chatRepository: ChatRepository
+class CreateNewChatRoomUseCase
+@Inject
+constructor(
+    private val chatRepository: ChatRepository,
 ) {
     suspend operator fun invoke(request: AddChatRequest) =
         chatRepository.createNewChatRoom(request)
