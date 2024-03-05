d6275c5e0cb1e7c39760c86309aadd1b0d63887b
thsamajiki <ant1102@naver.com>
2024-03-05 11:19:50 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/rate/usecase/GetRateUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/rate/usecase/GetRateUseCase.kt
@@ -4,10 +4,15 @@ import com.hero.recipespace.domain.rate.entity.RateEntity
 import com.hero.recipespace.domain.rate.repository.RateRepository
 import javax.inject.Inject
 
-class GetRateUseCase @Inject constructor(
-    private val rateRepository: RateRepository
+class GetRateUseCase
+@Inject
+constructor(
+    private val rateRepository: RateRepository,
 ) {
-    suspend operator fun invoke(userKey: String, recipeKey: String): Result<RateEntity> =
+    suspend operator fun invoke(
+        userKey: String,
+        recipeKey: String,
+    ): Result<RateEntity> =
         kotlin.runCatching {
             rateRepository.getRate(userKey, recipeKey)
         }
