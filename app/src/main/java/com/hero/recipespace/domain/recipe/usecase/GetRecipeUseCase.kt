63fcb7be68f299fb1ef4f36f1c4053c5c2f9a334
thsamajiki <ant1102@naver.com>
2024-03-05 11:19:55 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/recipe/usecase/GetRecipeUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/recipe/usecase/GetRecipeUseCase.kt
@@ -4,8 +4,10 @@ import com.hero.recipespace.domain.recipe.entity.RecipeEntity
 import com.hero.recipespace.domain.recipe.repository.RecipeRepository
 import javax.inject.Inject
 
-class GetRecipeUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+class GetRecipeUseCase
+@Inject
+constructor(
+    private val recipeRepository: RecipeRepository,
 ) {
     suspend operator fun invoke(recipeKey: String): Result<RecipeEntity> =
         kotlin.runCatching {
