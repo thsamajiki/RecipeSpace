28b3a2145d952cf67c26ce4a78e1854a93c7ab78
thsamajiki <ant1102@naver.com>
2024-03-05 11:21:02 +0900

    ktlint 따른 라인 정렬

--- app/src/main/java/com/hero/recipespace/view/LoadingState.kt
+++ app/src/main/java/com/hero/recipespace/view/LoadingState.kt
@@ -2,10 +2,10 @@ package com.hero.recipespace.view
 
 sealed class LoadingState {
     object Loading : LoadingState()
-    
+
     data class Progress(val value: Int) : LoadingState()
-    
-    object Hidden: LoadingState()
-    
-    object Idle: LoadingState()
+
+    object Hidden : LoadingState()
+
+    object Idle : LoadingState()
 }
