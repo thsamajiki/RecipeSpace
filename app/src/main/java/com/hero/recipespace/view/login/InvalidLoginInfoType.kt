7b9d71d03441698a5bf411cd03156c9d843913f9
thsamajiki <ant1102@naver.com>
2024-03-05 11:20:25 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/view/login/InvalidLoginInfoType.kt
+++ app/src/main/java/com/hero/recipespace/view/login/InvalidLoginInfoType.kt
@@ -3,9 +3,9 @@ package com.hero.recipespace.view.login
 import com.hero.recipespace.R
 
 enum class InvalidLoginInfoType(
-    private val message: Int
+    private val message: Int,
 ) {
     INVALID_EMAIL_FORM(R.string.invalid_email_form),
     INCORRECT_PWD(R.string.incorrect_password),
-    EMPTY_PWD(R.string.empty_password);
+    EMPTY_PWD(R.string.empty_password),
 }
