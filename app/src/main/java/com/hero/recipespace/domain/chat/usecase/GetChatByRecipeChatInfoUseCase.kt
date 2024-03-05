ba93c00ad3e560e4274a3f622f78e24f9d372307
thsamajiki <ant1102@naver.com>
2024-03-05 11:19:19 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/chat/usecase/GetChatByRecipeChatInfoUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/chat/usecase/GetChatByRecipeChatInfoUseCase.kt
@@ -5,10 +5,12 @@ import com.hero.recipespace.domain.chat.repository.ChatRepository
 import com.hero.recipespace.view.main.chat.RecipeChatInfo
 import javax.inject.Inject
 
-class GetChatByRecipeChatInfoUseCase @Inject constructor(
-    private val chatRepository: ChatRepository
+class GetChatByRecipeChatInfoUseCase
+@Inject
+constructor(
+    private val chatRepository: ChatRepository,
 ) {
-    suspend operator fun invoke(chatInfo: RecipeChatInfo) : Result<ChatEntity> =
+    suspend operator fun invoke(chatInfo: RecipeChatInfo): Result<ChatEntity> =
         kotlin.runCatching {
             chatRepository.getChatByRecipeChatInfo(chatInfo)
         }
