162af93b851d8563238b83ff735c9adb19d71012
thsamajiki <ant1102@naver.com>
2024-03-05 11:18:49 +0900

    ktlint 따른 라인 정렬

--- app/src/main/java/com/hero/recipespace/domain/exception/FailedCreateUserException.kt
+++ app/src/main/java/com/hero/recipespace/domain/exception/FailedCreateUserException.kt
@@ -1,4 +1,3 @@
 package com.hero.recipespace.domain.exception
 
-class FailedCreateUserException(message: String, cause: Throwable): Exception() {
-}
+class FailedCreateUserException(message: String, cause: Throwable) : Exception()
