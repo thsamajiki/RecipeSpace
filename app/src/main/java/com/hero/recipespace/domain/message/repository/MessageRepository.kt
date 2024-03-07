869613cd76a8fb93df78ae472db476dd4a2b1705
thsamajiki <ant1102@naver.com>
2024-03-07 10:54:02 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/message/repository/MessageRepository.kt
+++ app/src/main/java/com/hero/recipespace/domain/message/repository/MessageRepository.kt
@@ -5,15 +5,24 @@ import com.hero.recipespace.domain.message.request.ReadMessageRequest
 import kotlinx.coroutines.flow.Flow
 
 interface MessageRepository {
-    suspend fun getMessage(messageKey: String) : MessageEntity
+    suspend fun getMessage(messageKey: String): MessageEntity
 
     suspend fun readMessage(request: ReadMessageRequest): Boolean
 
-    fun getMessageList(chatKey: String) : Flow<List<MessageEntity>>
+    fun getMessageList(chatKey: String): Flow<List<MessageEntity>>
 
-    suspend fun addMessage(chatKey: String, message: String)
+    suspend fun addMessage(
+        chatKey: String,
+        message: String,
+    )
 
-    suspend fun modifyMessage(chatKey: String, message: String)
+    suspend fun modifyMessage(
+        chatKey: String,
+        message: String,
+    )
 
-    suspend fun deleteMessage(chatKey: String, message: String)
+    suspend fun deleteMessage(
+        chatKey: String,
+        message: String,
+    )
 }
