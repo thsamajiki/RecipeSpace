7241b68386f7b0d249814068eb23232022a2dd0e
thsamajiki <ant1102@naver.com>
2024-03-07 10:55:04 +0900

    ktlint에 따른 라인 정렬, 와일드 import 제거

--- app/src/main/java/com/hero/recipespace/database/notice/dao/NoticeDao.kt
+++ app/src/main/java/com/hero/recipespace/database/notice/dao/NoticeDao.kt
@@ -1,13 +1,18 @@
 package com.hero.recipespace.database.notice.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.hero.recipespace.data.notice.NoticeData
 
 @Dao
 interface NoticeDao {
     @Query("SELECT * FROM notice_db ORDER BY `key` ASC")
-    fun getAllNotices() : LiveData<List<NoticeData>>
+    fun getAllNotices(): LiveData<List<NoticeData>>
 
     @Query("SELECT * FROM notice_db WHERE `key` = :key limit 1")
     suspend fun getNoticeFromKey(key: String?): NoticeData?
