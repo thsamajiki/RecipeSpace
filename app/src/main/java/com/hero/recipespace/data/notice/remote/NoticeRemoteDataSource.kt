9599072705717287b9c8996d509c033280c099ce
thsamajiki <ant1102@naver.com>
2024-03-07 10:57:09 +0900

    ktlint에 따른 라인 정렬

--- app/src/main/java/com/hero/recipespace/data/notice/remote/NoticeRemoteDataSource.kt
+++ app/src/main/java/com/hero/recipespace/data/notice/remote/NoticeRemoteDataSource.kt
@@ -4,9 +4,9 @@ import com.hero.recipespace.data.notice.NoticeData
 import kotlinx.coroutines.flow.Flow
 
 interface NoticeRemoteDataSource {
-    suspend fun getData(noticeKey: String) : NoticeData
+    suspend fun getData(noticeKey: String): NoticeData
 
-    fun getDataList() : Flow<List<NoticeData>>
+    fun getDataList(): Flow<List<NoticeData>>
 
     suspend fun add(noticeData: NoticeData)
 
