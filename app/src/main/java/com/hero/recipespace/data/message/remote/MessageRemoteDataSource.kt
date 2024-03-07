ecd607e4a43493551546b1466cb67253cf606c91
thsamajiki <ant1102@naver.com>
2024-03-07 10:53:48 +0900

    ktlint 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/message/remote/MessageRemoteDataSource.kt
+++ app/src/main/java/com/hero/recipespace/data/message/remote/MessageRemoteDataSource.kt
@@ -11,9 +11,18 @@ interface MessageRemoteDataSource {
 
     suspend fun getDataList(chatKey: String): Flow<List<MessageData>>
 
-    suspend fun add(chatKey: String, message: String): MessageData
+    suspend fun add(
+        chatKey: String,
+        message: String,
+    ): MessageData
 
-    suspend fun update(chatKey: String, message: String): MessageData
+    suspend fun update(
+        chatKey: String,
+        message: String,
+    ): MessageData
 
-    suspend fun remove(chatKey: String, message: String): MessageData
+    suspend fun remove(
+        chatKey: String,
+        message: String,
+    ): MessageData
 }
