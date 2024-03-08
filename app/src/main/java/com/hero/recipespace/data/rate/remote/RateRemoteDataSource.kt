3e92ed38c36b23ad1b10110ca65a346b428b741a
thsamajiki <ant1102@naver.com>
2024-03-08 23:36:32 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/rate/remote/RateRemoteDataSource.kt
+++ app/src/main/java/com/hero/recipespace/data/rate/remote/RateRemoteDataSource.kt
@@ -5,11 +5,17 @@ import com.hero.recipespace.data.recipe.RecipeData
 import com.hero.recipespace.domain.rate.request.UpdateRateRequest
 
 interface RateRemoteDataSource {
-    suspend fun getData(userKey: String, recipeKey: String) : RateData
+    suspend fun getData(
+        userKey: String,
+        recipeKey: String,
+    ): RateData
 
-    suspend fun getDataList() : List<RateData>
+    suspend fun getDataList(): List<RateData>
 
-    suspend fun update(request: UpdateRateRequest, recipeData: RecipeData) : RateData
+    suspend fun update(
+        request: UpdateRateRequest,
+        recipeData: RecipeData,
+    ): RateData
 
-    suspend fun remove(rateKey: String) : RateData
+    suspend fun remove(rateKey: String): RateData
 }
