bbb2ea5f17d7d2e928dbd367647dc230cdb8300b
thsamajiki <ant1102@naver.com>
2024-03-08 23:33:40 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/listener/OnRecyclerItemClickListener.kt
+++ app/src/main/java/com/hero/recipespace/listener/OnRecyclerItemClickListener.kt
@@ -3,5 +3,9 @@ package com.hero.recipespace.listener
 import android.view.View
 
 interface OnRecyclerItemClickListener<T> {
-    fun onItemClick(position: Int, view: View, data: T)
+    fun onItemClick(
+        position: Int,
+        view: View,
+        data: T,
+    )
 }
