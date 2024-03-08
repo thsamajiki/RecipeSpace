38ecc8ac124bf26202179cbfc270f1add59a3f31
thsamajiki <ant1102@naver.com>
2024-03-08 23:33:25 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/recipe/usecase/ObserveRecipeListUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/recipe/usecase/ObserveRecipeListUseCase.kt
@@ -5,9 +5,11 @@ import com.hero.recipespace.domain.recipe.repository.RecipeRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class ObserveRecipeListUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+class ObserveRecipeListUseCase
+@Inject
+constructor(
+    private val recipeRepository: RecipeRepository,
 ) {
-    operator fun invoke() : Flow<List<RecipeEntity>> =
+    operator fun invoke(): Flow<List<RecipeEntity>> =
         recipeRepository.observeRecipeList()
 }
