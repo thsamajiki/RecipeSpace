689953e92b51d15192ca4bd35eb1a369a09f26bd
thsamajiki <ant1102@naver.com>
2024-03-08 23:36:38 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/rate/remote/RateRemoteDataSourceImpl.kt
+++ app/src/main/java/com/hero/recipespace/data/rate/remote/RateRemoteDataSourceImpl.kt
@@ -6,22 +6,36 @@ import com.hero.recipespace.data.recipe.RecipeData
 import com.hero.recipespace.domain.rate.request.UpdateRateRequest
 import javax.inject.Inject
 
-class RateRemoteDataSourceImpl @Inject constructor(
-    private val rateService: RateService
+class RateRemoteDataSourceImpl
+@Inject
+constructor(
+    private val rateService: RateService,
 ) : RateRemoteDataSource {
-    override suspend fun getData(userKey: String, recipeKey: String): RateData {
-        return rateService.getData(userKey, recipeKey)
+    override suspend fun getData(
+        userKey: String,
+        recipeKey: String,
+    ): RateData {
+        return rateService.getData(
+            userKey,
+            recipeKey,
+        )
     }
 
     override suspend fun getDataList(): List<RateData> {
         return rateService.getDataList()
     }
 
-    override suspend fun update(request: UpdateRateRequest, recipeData: RecipeData) : RateData  {
-        return rateService.update(request, recipeData)
+    override suspend fun update(
+        request: UpdateRateRequest,
+        recipeData: RecipeData,
+    ): RateData {
+        return rateService.update(
+            request,
+            recipeData,
+        )
     }
 
-    override suspend fun remove(rateKey: String) : RateData {
+    override suspend fun remove(rateKey: String): RateData {
         return rateService.remove(rateKey)
     }
 }
