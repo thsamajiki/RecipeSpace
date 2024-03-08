9d0a12addbd18d1217ebef910543055decd4823d
thsamajiki <ant1102@naver.com>
2024-03-08 23:35:45 +0900

    ktlint에 따른 라인 정렬, 와일드 import 제거

--- app/src/main/java/com/hero/recipespace/database/rate/dao/RateDao.kt
+++ app/src/main/java/com/hero/recipespace/database/rate/dao/RateDao.kt
@@ -1,13 +1,18 @@
 package com.hero.recipespace.database.rate.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.hero.recipespace.data.rate.RateData
 
 @Dao
 interface RateDao {
     @Query("SELECT * FROM rate_db ORDER BY `rateKey` ASC")
-    fun getAllRates() : LiveData<List<RateData>>
+    fun getAllRates(): LiveData<List<RateData>>
 
     @Query("SELECT * FROM rate_db WHERE `rateKey` = :key limit 1")
     suspend fun getRateFromKey(key: String?): RateData?
