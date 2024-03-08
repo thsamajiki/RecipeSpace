717bf7a533b1212d8bbac053ee5fecee9d864c97
thsamajiki <ant1102@naver.com>
2024-03-08 23:33:19 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/notice/usecase/ObserveNoticeListUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/notice/usecase/ObserveNoticeListUseCase.kt
@@ -5,9 +5,11 @@ import com.hero.recipespace.domain.notice.repository.NoticeRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class ObserveNoticeListUseCase @Inject constructor(
-    private val noticeRepository: NoticeRepository
+class ObserveNoticeListUseCase
+@Inject
+constructor(
+    private val noticeRepository: NoticeRepository,
 ) {
-    operator fun invoke() : Flow<List<NoticeEntity>> =
+    operator fun invoke(): Flow<List<NoticeEntity>> =
         noticeRepository.getNoticeList()
 }
