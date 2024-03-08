1d4a4a5a9e62ea0bbaf6ae2495f38322d541801a
thsamajiki <ant1102@naver.com>
2024-03-08 23:36:43 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/rate/repository/RateRepository.kt
+++ app/src/main/java/com/hero/recipespace/domain/rate/repository/RateRepository.kt
@@ -6,13 +6,19 @@ import com.hero.recipespace.domain.recipe.entity.RecipeEntity
 import kotlinx.coroutines.flow.Flow
 
 interface RateRepository {
-    suspend fun getRate(rateKey: String) : RateEntity
+    suspend fun getRate(rateKey: String): RateEntity
 
-    suspend fun getRate(userKey: String, recipeKey: String): RateEntity
+    suspend fun getRate(
+        userKey: String,
+        recipeKey: String,
+    ): RateEntity
 
-    fun observeRateList() : Flow<List<RateEntity>>
+    fun observeRateList(): Flow<List<RateEntity>>
 
-    suspend fun submitRate(request: UpdateRateRequest, recipeEntity: RecipeEntity) : RateEntity
+    suspend fun submitRate(
+        request: UpdateRateRequest,
+        recipeEntity: RecipeEntity,
+    ): RateEntity
 
     suspend fun deleteRate(rateKey: String)
 }
