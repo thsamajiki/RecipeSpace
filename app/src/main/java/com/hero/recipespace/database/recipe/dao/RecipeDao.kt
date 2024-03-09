7c3c874614f6af936b4857b7b416618952ae2457
thsamajiki <ant1102@naver.com>
2024-03-09 23:33:20 +0900

    ktlint에 따른 라인 정렬, 와일드 import 제거

--- app/src/main/java/com/hero/recipespace/database/recipe/dao/RecipeDao.kt
+++ app/src/main/java/com/hero/recipespace/database/recipe/dao/RecipeDao.kt
@@ -1,13 +1,18 @@
 package com.hero.recipespace.database.recipe.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.hero.recipespace.data.recipe.RecipeData
 
 @Dao
 interface RecipeDao {
     @Query("SELECT * FROM recipe_db ORDER BY postDate DESC")
-    fun observeAllRecipes() : LiveData<List<RecipeData>> //
+    fun observeAllRecipes(): LiveData<List<RecipeData>>
 
     @Query("SELECT * FROM recipe_db WHERE `key` = :key limit 1")
     suspend fun getRecipeFromKey(key: String?): RecipeData?
