6e666f4c52fc54ea15e69198dafb81ee09fe2e75
thsamajiki <ant1102@naver.com>
2024-03-09 23:32:13 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/message/usecase/ReadMessageUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/message/usecase/ReadMessageUseCase.kt
@@ -4,8 +4,10 @@ import com.hero.recipespace.domain.message.repository.MessageRepository
 import com.hero.recipespace.domain.message.request.ReadMessageRequest
 import javax.inject.Inject
 
-class ReadMessageUseCase @Inject constructor(
-    private val messageRepository: MessageRepository
+class ReadMessageUseCase
+@Inject
+constructor(
+    private val messageRepository: MessageRepository,
 ) {
     suspend operator fun invoke(request: ReadMessageRequest) =
         messageRepository.readMessage(request)
