4ea318d570d5bb4a5d00d7613f42c95c992e180c
thsamajiki <ant1102@naver.com>
2024-03-10 21:38:37 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/recipe/remote/RecipeRemoteDataSource.kt
+++ app/src/main/java/com/hero/recipespace/data/recipe/remote/RecipeRemoteDataSource.kt
@@ -9,9 +9,15 @@ interface RecipeRemoteDataSource {
 
     suspend fun getDataList(): List<RecipeData>
 
-    suspend fun add(request: UploadRecipeRequest, onProgress: (Float) -> Unit): RecipeData
+    suspend fun add(
+        request: UploadRecipeRequest,
+        onProgress: (Float) -> Unit,
+    ): RecipeData
 
-    suspend fun update(request: UpdateRecipeRequest, onProgress: (Float) -> Unit): RecipeData
+    suspend fun update(
+        request: UpdateRecipeRequest,
+        onProgress: (Float) -> Unit,
+    ): RecipeData
 
     suspend fun remove(recipeData: RecipeData): RecipeData
 }
