e2bea3e71c6121dc30c0abd6bcef0ca04c019c72
thsamajiki <ant1102@naver.com>
2024-03-10 21:39:22 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/recipe/service/RecipeServiceImpl.kt
+++ app/src/main/java/com/hero/recipespace/data/recipe/service/RecipeServiceImpl.kt
@@ -22,11 +22,12 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class RecipeServiceImpl @Inject constructor(
+class RecipeServiceImpl
+@Inject
+constructor(
     private val db: FirebaseFirestore,
-    private val storage: FirebaseStorage
+    private val storage: FirebaseStorage,
 ) : RecipeService {
-
     override suspend fun getRecipe(recipeKey: String): RecipeData {
         val rateAverage = getRecipeRateAverage(recipeKey)
 
@@ -39,8 +40,9 @@ class RecipeServiceImpl @Inject constructor(
                         return@addOnSuccessListener
                     }
 
-                    val recipeData = documentSnapshot.toObject(RecipeData::class.java)
-                        ?.copy(rate = rateAverage)
+                    val recipeData =
+                        documentSnapshot.toObject(RecipeData::class.java)
+                            ?.copy(rate = rateAverage)
 
                     continuation.resume(recipeData!!)
                 }
@@ -52,21 +54,25 @@ class RecipeServiceImpl @Inject constructor(
 
     private suspend fun getRecipeRateAverage(recipeKey: String): Float {
         return suspendCoroutine { continuation ->
-            val recipeRef = db.collection("Recipe").document(recipeKey)
+            val recipeRef =
+                db.collection("Recipe")
+                    .document(recipeKey)
             val rateRef = recipeRef.collection("RateList")
 
             rateRef.get()
                 .addOnSuccessListener { querySnapShot ->
-
-                    val rateDataList = querySnapShot.documents.mapNotNull { documentSnapshot ->
-                        documentSnapshot.toObject(RateData::class.java)
-                    }
+                    val rateDataList =
+                        querySnapShot.documents.mapNotNull { documentSnapshot ->
+                            documentSnapshot.toObject(RateData::class.java)
+                        }
 
                     WLog.d("rateDataList $rateDataList")
 
                     val rateAverage =
                         if (rateDataList.isNotEmpty()) {
-                            rateDataList.sumOf { it.rate?.toInt() ?: 0 } / rateDataList.size.toFloat()
+                            rateDataList.sumOf {
+                                it.rate?.toInt() ?: 0
+                            } / rateDataList.size.toFloat()
                         } else {
                             0f
                         }
@@ -82,31 +88,38 @@ class RecipeServiceImpl @Inject constructor(
     override suspend fun getRecipeList(): List<RecipeData> {
         return suspendCoroutine { continuation ->
             db.collection("Recipe")
-                .orderBy("postDate", Query.Direction.DESCENDING)
+                .orderBy(
+                    "postDate",
+                    Query.Direction.DESCENDING,
+                )
                 .get()
-                .addOnSuccessListener(OnSuccessListener { queryDocumentSnapshots ->
-                    if (queryDocumentSnapshots.isEmpty) {
-                        return@OnSuccessListener
-                    }
-                    val recipeDataList = queryDocumentSnapshots.documents
-                        .mapNotNull { documentSnapshot ->
-                            documentSnapshot.toObject(RecipeData::class.java)
+                .addOnSuccessListener(
+                    OnSuccessListener { queryDocumentSnapshots ->
+                        if (queryDocumentSnapshots.isEmpty) {
+                            return@OnSuccessListener
                         }
+                        val recipeDataList =
+                            queryDocumentSnapshots.documents
+                                .mapNotNull { documentSnapshot ->
+                                    documentSnapshot.toObject(RecipeData::class.java)
+                                }
 
-                    CoroutineScope(Dispatchers.Main).launch {
-                        val result = recipeDataList.map { recipe ->
-                            async {
-                                val rateAverage = getRecipeRateAverage(recipe.key)
-                                recipe.copy(rate = rateAverage)
-                            }
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val result =
+                                recipeDataList.map { recipe ->
+                                    async {
+                                        val rateAverage = getRecipeRateAverage(recipe.key)
+                                        recipe.copy(rate = rateAverage)
+                                    }
+                                }
+                                    .awaitAll()
+
+                            continuation.resume(result)
+                            WLog.d("result $result")
+                            cancel()
                         }
-                            .awaitAll()
-
-                        continuation.resume(result)
-                        WLog.d("result $result")
-                        cancel()
-                    }
-                })
+                    },
+                )
                 .addOnFailureListener {
                     continuation.resumeWithException(it)
                 }
@@ -116,31 +129,38 @@ class RecipeServiceImpl @Inject constructor(
     override suspend fun getMyRecipeList(userKey: String): List<RecipeData> {
         return suspendCoroutine { continuation ->
             db.collection("Recipe")
-                .whereEqualTo("userKey", userKey)
+                .whereEqualTo(
+                    "userKey",
+                    userKey,
+                )
                 .get()
-                .addOnSuccessListener(OnSuccessListener { queryDocumentSnapshots ->
-                    if (queryDocumentSnapshots.isEmpty) {
-                        return@OnSuccessListener
-                    }
-                    val recipeDataList = queryDocumentSnapshots.documents
-                        .mapNotNull { documentSnapshot ->
-                            documentSnapshot.toObject(RecipeData::class.java)
+                .addOnSuccessListener(
+                    OnSuccessListener { queryDocumentSnapshots ->
+                        if (queryDocumentSnapshots.isEmpty) {
+                            return@OnSuccessListener
                         }
+                        val recipeDataList =
+                            queryDocumentSnapshots.documents
+                                .mapNotNull { documentSnapshot ->
+                                    documentSnapshot.toObject(RecipeData::class.java)
+                                }
 
-                    CoroutineScope(Dispatchers.Main).launch {
-                        val result = recipeDataList.map { recipe ->
-                            async {
-                                val rateAverage = getRecipeRateAverage(recipe.key)
-                                recipe.copy(rate = rateAverage)
-                            }
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val result =
+                                recipeDataList.map { recipe ->
+                                    async {
+                                        val rateAverage = getRecipeRateAverage(recipe.key)
+                                        recipe.copy(rate = rateAverage)
+                                    }
+                                }
+                                    .awaitAll()
+
+                            continuation.resume(result)
+
+                            cancel()
                         }
-                            .awaitAll()
-
-                        continuation.resume(result)
-
-                        cancel()
-                    }
-                })
+                    },
+                )
                 .addOnFailureListener {
                     continuation.resumeWithException(it)
                 }
@@ -153,20 +173,22 @@ class RecipeServiceImpl @Inject constructor(
         postDate: Timestamp,
         userKey: String,
         userName: String,
-        profileImageUrl : String
+        profileImageUrl: String,
     ): RecipeData {
         return suspendCoroutine<RecipeData> { continuation ->
-            val documentReference = db.collection("Recipe").document()
+            val documentReference =
+                db.collection("Recipe").document()
             val key = documentReference.id
-            val recipeData = RecipeData(
-                key,
-                profileImageUrl,
-                userName,
-                userKey,
-                desc,
-                photoUrlList,
-                postDate
-            )
+            val recipeData =
+                RecipeData(
+                    key,
+                    profileImageUrl,
+                    userName,
+                    userKey,
+                    desc,
+                    photoUrlList,
+                    postDate,
+                )
 
             documentReference.set(recipeData)
                 .addOnSuccessListener { continuation.resume(recipeData) }
@@ -176,43 +198,46 @@ class RecipeServiceImpl @Inject constructor(
 
     override suspend fun uploadImages(
         recipePhotoPathList: List<String>,
-        progress: (Float) -> Unit
+        progress: (Float) -> Unit,
     ): List<String> {
-        val oldImageList = recipePhotoPathList.filter {
-            it.startsWith("https://") || it.startsWith("http://")
-        }
+        val oldImageList =
+            recipePhotoPathList.filter {
+                it.startsWith("https://") || it.startsWith("http://")
+            }
 
-        val pathList = recipePhotoPathList.filterNot {
-            it.startsWith("https://") || it.startsWith("http://")
-        }
+        val pathList =
+            recipePhotoPathList.filterNot {
+                it.startsWith("https://") || it.startsWith("http://")
+            }
 
         val imageFolderRef = storage.reference.child(DEFAULT_IMAGE_PATH)
 
         return withContext(Dispatchers.IO) {
-            val newImageList = pathList.map { photoPath ->
-                val imageRef = imageFolderRef.child("${Uri.parse(photoPath).lastPathSegment}")
+            val newImageList =
+                pathList.map { photoPath ->
+                    val imageRef = imageFolderRef.child("${Uri.parse(photoPath).lastPathSegment}")
 
-                async {
-                    kotlin.runCatching {
-                        val imageFile = Uri.parse(photoPath)
-                        val uploadTask = imageRef.putFile(imageFile)
+                    async {
+                        kotlin.runCatching {
+                            val imageFile = Uri.parse(photoPath)
+                            val uploadTask = imageRef.putFile(imageFile)
 
-                        uploadTask
-                            .await()
-                            .storage
-                            .downloadUrl
-                            .await()
-                            .toString()
-                    }
-                        .onFailure {
-                            WLog.e(it)
+                            uploadTask
+                                .await()
+                                .storage
+                                .downloadUrl
+                                .await()
+                                .toString()
                         }
+                            .onFailure {
+                                WLog.e(it)
+                            }
+                    }
                 }
-            }
-                .awaitAll()
-                .mapNotNull {
-                    it.getOrNull()
-                }
+                    .awaitAll()
+                    .mapNotNull {
+                        it.getOrNull()
+                    }
 
             oldImageList + newImageList
         }
@@ -222,7 +247,7 @@ class RecipeServiceImpl @Inject constructor(
         key: String,
         content: String,
         photoUrlList: List<String>,
-        onProgress: (Float) -> Unit
+        onProgress: (Float) -> Unit,
     ): RecipeData {
         val editData = HashMap<String, Any>()
 
