7869f735cb221d9408d41b01489f598cd68d83a6
thsamajiki <ant1102@naver.com>
2024-03-10 21:39:13 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/data/recipe/service/RecipeService.kt
+++ app/src/main/java/com/hero/recipespace/data/recipe/service/RecipeService.kt
@@ -5,23 +5,31 @@ import com.hero.recipespace.data.recipe.RecipeData
 
 interface RecipeService {
     suspend fun getRecipe(recipeKey: String): RecipeData
-    
+
     suspend fun getRecipeList(): List<RecipeData>
 
     suspend fun getMyRecipeList(userKey: String): List<RecipeData>
 
-    suspend fun add(desc: String,
-                    photoUrlList: List<String>,
-                    postDate: Timestamp,
-                    userKey: String,
-                    userName: String,
-                    profileImageUrl : String
-    ) : RecipeData
+    suspend fun add(
+        desc: String,
+        photoUrlList: List<String>,
+        postDate: Timestamp,
+        userKey: String,
+        userName: String,
+        profileImageUrl: String,
+    ): RecipeData
 
-    suspend fun uploadImages(recipePhotoPathList: List<String>, progress: (Float) -> Unit): List<String>
-    
-    suspend fun update(key: String,
-                       content: String, photoUrlList: List<String>, onProgress: (Float) -> Unit) : RecipeData
-    
-    suspend fun remove(recipeData: RecipeData) : RecipeData
+    suspend fun uploadImages(
+        recipePhotoPathList: List<String>,
+        progress: (Float) -> Unit,
+    ): List<String>
+
+    suspend fun update(
+        key: String,
+        content: String,
+        photoUrlList: List<String>,
+        onProgress: (Float) -> Unit,
+    ): RecipeData
+
+    suspend fun remove(recipeData: RecipeData): RecipeData
 }
