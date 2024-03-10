66d63f05db99f26cbc25b1d9c6d05d804b5b1cc3
thsamajiki <ant1102@naver.com>
2024-03-10 21:40:11 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/chat/usecase/RefreshChatListUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/chat/usecase/RefreshChatListUseCase.kt
@@ -3,8 +3,10 @@ package com.hero.recipespace.domain.chat.usecase
 import com.hero.recipespace.domain.chat.repository.ChatRepository
 import javax.inject.Inject
 
-class RefreshChatListUseCase @Inject constructor(
-    private val chatRepository: ChatRepository
+class RefreshChatListUseCase
+@Inject
+constructor(
+    private val chatRepository: ChatRepository,
 ) {
     suspend operator fun invoke(userKey: String): Result<Unit> {
         return kotlin.runCatching { chatRepository.refresh(userKey) }
