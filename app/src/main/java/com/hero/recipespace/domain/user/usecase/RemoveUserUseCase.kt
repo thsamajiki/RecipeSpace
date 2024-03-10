2c5911ec8e3653940ad2ff7050b96d9e94f01614
thsamajiki <ant1102@naver.com>
2024-03-10 21:40:26 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/user/usecase/RemoveUserUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/user/usecase/RemoveUserUseCase.kt
@@ -4,8 +4,10 @@ import com.hero.recipespace.domain.user.entity.UserEntity
 import com.hero.recipespace.domain.user.repository.UserRepository
 import javax.inject.Inject
 
-class RemoveUserUseCase @Inject constructor(
-    private val userRepository: UserRepository
+class RemoveUserUseCase
+@Inject
+constructor(
+    private val userRepository: UserRepository,
 ) {
     suspend operator fun invoke(userEntity: UserEntity): Result<Unit> =
         kotlin.runCatching { userRepository.deleteUser(userEntity) }
