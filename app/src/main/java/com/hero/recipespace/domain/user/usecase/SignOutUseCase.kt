6bad3ed2f30c3876437f8471e544e0da2afe7aab
thsamajiki <ant1102@naver.com>
2024-03-13 17:48:38 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/user/usecase/SignOutUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/user/usecase/SignOutUseCase.kt
@@ -3,8 +3,10 @@ package com.hero.recipespace.domain.user.usecase
 import com.hero.recipespace.domain.user.repository.UserRepository
 import javax.inject.Inject
 
-class SignOutUseCase @Inject constructor(
-    private val userRepository: UserRepository
+class SignOutUseCase
+@Inject
+constructor(
+    private val userRepository: UserRepository,
 ) {
     suspend operator fun invoke(): Result<Unit> =
         kotlin.runCatching { userRepository.signOut() }
