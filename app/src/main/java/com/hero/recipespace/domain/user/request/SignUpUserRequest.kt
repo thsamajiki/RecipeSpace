56a5a588617e53abff876dd2e4447d75ede1ce58
thsamajiki <ant1102@naver.com>
2024-03-13 17:48:51 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/user/request/SignUpUserRequest.kt
+++ app/src/main/java/com/hero/recipespace/domain/user/request/SignUpUserRequest.kt
@@ -6,12 +6,7 @@ import com.hero.recipespace.domain.user.entity.Password
 data class SignUpUserRequest(
     val email: Email,
     val name: String,
-    val pwd: Password
+    val pwd: Password,
 )
-
-
-
-
-
 // java clean code
 // 타입 분리도 하고 메모리 이슈도 해결함
