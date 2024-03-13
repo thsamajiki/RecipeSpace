8bfd4a3232d869737286824c71a81d5420408e47
thsamajiki <ant1102@naver.com>
2024-03-13 17:51:02 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/recipe/request/UploadRecipeRequest.kt
+++ app/src/main/java/com/hero/recipespace/domain/recipe/request/UploadRecipeRequest.kt
@@ -2,5 +2,5 @@ package com.hero.recipespace.domain.recipe.request
 
 data class UploadRecipeRequest(
     val content: String,
-    val recipePhotoPathList: List<String>
+    val recipePhotoPathList: List<String>,
 )
