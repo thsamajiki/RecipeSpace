025065eb1ae6559de5b2c47020c7e2691f7db377
thsamajiki <ant1102@naver.com>
2024-03-13 17:50:45 +0900

    ktlint에 따른 라인 정렬, 콤마 추가

--- app/src/main/java/com/hero/recipespace/domain/recipe/usecase/UpdateRecipeUseCase.kt
+++ app/src/main/java/com/hero/recipespace/domain/recipe/usecase/UpdateRecipeUseCase.kt
@@ -5,13 +5,15 @@ import com.hero.recipespace.domain.recipe.repository.RecipeRepository
 import com.hero.recipespace.domain.recipe.request.UpdateRecipeRequest
 import javax.inject.Inject
 
-class UpdateRecipeUseCase @Inject constructor(
-    private val recipeRepository: RecipeRepository
+class UpdateRecipeUseCase
+@Inject
+constructor(
+    private val recipeRepository: RecipeRepository,
 ) {
     suspend operator fun invoke(
         request: UpdateRecipeRequest,
-        onProgress: (Float) -> Unit
-    ) : Result<RecipeEntity> =
+        onProgress: (Float) -> Unit,
+    ): Result<RecipeEntity> =
         kotlin.runCatching {
             recipeRepository.modifyRecipe(request, onProgress)
         }
