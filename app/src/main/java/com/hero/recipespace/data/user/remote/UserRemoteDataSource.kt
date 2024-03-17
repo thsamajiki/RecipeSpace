6357e3a7b56ba49f9b8faf9201b69487a8537318
thsamajiki <ant1102@naver.com>
2024-03-17 18:21:23 +0900

    ktlint에 따른 라인 정렬

--- app/src/main/java/com/hero/recipespace/data/user/remote/UserRemoteDataSource.kt
+++ app/src/main/java/com/hero/recipespace/data/user/remote/UserRemoteDataSource.kt
@@ -6,19 +6,19 @@ import com.hero.recipespace.domain.user.request.SignUpUserRequest
 import com.hero.recipespace.domain.user.request.UpdateUserRequest
 
 interface UserRemoteDataSource {
-    suspend fun login(request: LoginUserRequest) : UserData
+    suspend fun login(request: LoginUserRequest): UserData
 
-    suspend fun getUserData(userKey: String) : UserData
+    suspend fun getUserData(userKey: String): UserData
 
     suspend fun getFirebaseAuthProfile(): UserData
 
-    suspend fun getDataList() : List<UserData>
+    suspend fun getDataList(): List<UserData>
 
-    suspend fun add(request: SignUpUserRequest) : UserData
+    suspend fun add(request: SignUpUserRequest): UserData
 
-    suspend fun updateUser(request: UpdateUserRequest) : UserData
+    suspend fun updateUser(request: UpdateUserRequest): UserData
 
-    suspend fun remove(userData: UserData) : UserData
+    suspend fun remove(userData: UserData): UserData
 
     suspend fun signOut()
 }
