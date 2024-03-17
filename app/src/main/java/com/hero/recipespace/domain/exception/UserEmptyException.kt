43777089f064328fea8b7dcec6c24eddcfae0efb
thsamajiki <ant1102@naver.com>
2024-03-17 18:20:28 +0900

    ktlint에 따른 라인 정렬

--- app/src/main/java/com/hero/recipespace/domain/exception/UserEmptyException.kt
+++ app/src/main/java/com/hero/recipespace/domain/exception/UserEmptyException.kt
@@ -1,4 +1,3 @@
 package com.hero.recipespace.domain.exception
 
-class UserEmptyException(message: String, cause: Throwable): Exception() {
-}
+class UserEmptyException(message: String, cause: Throwable) : Exception()
