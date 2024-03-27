3b512d802121504bacfcf004b56064c62cc4ea39
thsamajiki <ant1102@naver.com>
2024-03-28 01:12:15 +0900

    유효하지 않은 로그인 타입 추가

--- app/src/main/java/com/hero/recipespace/view/login/InvalidLoginInfoType.kt
+++ app/src/main/java/com/hero/recipespace/view/login/InvalidLoginInfoType.kt
@@ -1,11 +1,13 @@
 package com.hero.recipespace.view.login
 
+import androidx.annotation.StringRes
 import com.hero.recipespace.R
 
 enum class InvalidLoginInfoType(
-    private val message: Int,
+    @StringRes val message: Int,
 ) {
     INVALID_EMAIL_FORM(R.string.invalid_email_form),
-    INCORRECT_PWD(R.string.incorrect_password),
+    EMPTY_EMAIL(R.string.empty_email),
     EMPTY_PWD(R.string.empty_password),
+    FAILED_LOGIN(R.string.failed_login)
 }
